9f1705719b48aae27020c9ccf7b730c3f3bb9390
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-08 15:39:00 -0400

    logger MW function works properly'

--- server.js
+++ server.js
@@ -2,12 +2,32 @@ const express = require('express');
 
 const server = express();
 
+// initialize the logger middleware
+server.use(logger);
+
+
+// Sanity Test
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
 //custom middleware
 
-function logger(req, res, next) {}
+// logger logs to the console the following information about each request:
+// request method
+// request url
+// timestamp
+// will run on every request made to the API
+function logger(req, res, next) {
+  //Timestamp
+  req.requestTime = Date();
+
+  //console log the request method to the request url at the requested timestamp
+  console.log(`${req.method} to ${req.originalUrl} at ${req.requestTime}`);
+
+  //push the next middleware in the que
+  next();
+}
 
 module.exports = server;
